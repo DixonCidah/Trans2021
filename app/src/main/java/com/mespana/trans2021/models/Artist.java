384db039e56bb8c8aea12cac112d55b471162cc0
Viotty Remi <remi.viotty@etudiant.univ-rennes1.fr>
2021-01-25 10:31:41 +0100

    add getters

--- app/src/main/java/com/mespana/trans2021/models/Artist.java
+++ app/src/main/java/com/mespana/trans2021/models/Artist.java
@@ -23,5 +23,88 @@ public class Artist {
     private String deezer;
     private String cou_text_en;
     private String origine_pays1;
-    
+
+    public String getRecordid() {
+        return recordid;
+    }
+
+    public String getOrigine_ville1() {
+        return origine_ville1;
+    }
+
+    public String getSpotify() {
+        return spotify;
+    }
+
+    public long getPremiere_date_timestamp() {
+        return premiere_date_timestamp;
+    }
+
+    public String getCou_official_lang_code() {
+        return cou_official_lang_code;
+    }
+
+    public String getCou_onu_code() {
+        return cou_onu_code;
+    }
+
+    public String getArtistes() {
+        return artistes;
+    }
+
+    public String getCou_iso2_code() {
+        return cou_iso2_code;
+    }
+
+    public String getCou_iso3_code() {
+        return cou_iso3_code;
+    }
+
+    public double getGeo_point_2d_x() {
+        return geo_point_2d_x;
+    }
+
+    public double getGeo_point_2d_y() {
+        return geo_point_2d_y;
+    }
+
+    public String getPremiere_salle() {
+        return premiere_salle;
+    }
+
+    public String getCou_is_receiving_quest() {
+        return cou_is_receiving_quest;
+    }
+
+    public String getEdition() {
+        return edition;
+    }
+
+    public String getCou_text_sp() {
+        return cou_text_sp;
+    }
+
+    public String getPremiere_date() {
+        return premiere_date;
+    }
+
+    public String getCou_is_ilomember() {
+        return cou_is_ilomember;
+    }
+
+    public String getAnnee() {
+        return annee;
+    }
+
+    public String getDeezer() {
+        return deezer;
+    }
+
+    public String getCou_text_en() {
+        return cou_text_en;
+    }
+
+    public String getOrigine_pays1() {
+        return origine_pays1;
+    }
 }

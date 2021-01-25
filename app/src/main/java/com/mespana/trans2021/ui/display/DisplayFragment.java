fdcb38c7635c56afd3981df8c69752e789372acb
maevaI2! <maevaespana@orange.fr>
2021-01-25 14:38:47 +0100

    WIP display

--- app/src/main/java/com/mespana/trans2021/ui/display/DisplayFragment.java
+++ app/src/main/java/com/mespana/trans2021/ui/display/DisplayFragment.java
@@ -1,6 +1,42 @@
 package com.mespana.trans2021.ui.display;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+
+import com.mespana.trans2021.R;
+import com.mespana.trans2021.ui.main.PageViewModel;
+import com.mespana.trans2021.ui.main.PlaceholderFragment;
 
 public class DisplayFragment extends Fragment {
+
+    private static final String ARG_SECTION_NUMBER = "section_number";
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    public View onCreateView(
+            @NonNull LayoutInflater inflater, ViewGroup container,
+            Bundle savedInstanceState) {
+        View root = inflater.inflate(R.layout.fragment_main, container, false);
+        final TextView textView = root.findViewById(R.id.section_label);
+        /*pageViewModel.getText().observe(this, new Observer<String>() {
+            @Override
+            public void onChanged(@Nullable String s) {
+                textView.setText(s);
+            }
+        });*/
+        return root;
+    }
 }

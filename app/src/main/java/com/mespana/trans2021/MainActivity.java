270b4ad0b9a27b23d1f57da27c3327d160c5d978
Viotty Remi <remi.viotty@etudiant.univ-rennes1.fr>
2021-01-27 12:17:22 +0100

    toolbar + firebase connexion

--- app/src/main/java/com/mespana/trans2021/MainActivity.java
+++ app/src/main/java/com/mespana/trans2021/MainActivity.java
@@ -1,12 +1,30 @@
 package com.mespana.trans2021;
 
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.firebase.ui.auth.AuthUI;
+import com.firebase.ui.auth.IdpResponse;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.dialog.MaterialAlertDialogBuilder;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 import com.mespana.trans2021.databinding.ActivityMainBinding;
 import com.mespana.trans2021.services.JsonParsingService;
 
+import java.util.Arrays;
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
 
+    private static final int    RC_SIGN_IN = 35000,
+            RC_SIGN_OUT = 35001;
     private ActivityMainBinding binding;
 
     @Override
@@ -15,5 +33,66 @@ public class MainActivity extends AppCompatActivity {
         binding = ActivityMainBinding.inflate(getLayoutInflater());
         JsonParsingService.parseJson(this);
         setContentView(binding.getRoot());
+        binding.account.setText(R.string.signin);
+        binding.account.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                if (FirebaseAuth.getInstance().getCurrentUser() == null) {
+                    signIn();
+                }
+                else{
+                    signOut();
+                }
+            }
+        });
+    }
+
+    private void signOut() {
+        new MaterialAlertDialogBuilder(this)
+                .setTitle(getResources().getString(R.string.signout_dialog_title))
+                .setMessage(getResources().getString(R.string.signout_dialog_message))
+                .setPositiveButton(R.string.ok, (dialogInterface, i) -> {
+                    AuthUI.getInstance()
+                            .signOut(this)
+                            .addOnCompleteListener(task -> {
+                                binding.account.setText(R.string.signin);
+                            });
+                })
+                .setNegativeButton(R.string.cancel, (dialogInterface, i) -> {
+                })
+
+        .show();
+    }
+
+    private void signIn() {
+        List<AuthUI.IdpConfig> providers = Arrays.asList(new AuthUI.IdpConfig.EmailBuilder().build());
+                //new AuthUI.IdpConfig.GoogleBuilder().build());
+        startActivityForResult(
+                AuthUI.getInstance()
+                        .createSignInIntentBuilder()
+                        .setAvailableProviders(providers)
+                        .setLogo(R.drawable.ic_trans)
+                        .setTheme(R.style.Theme_Trans2021)      // Set theme
+                        .build(),
+                RC_SIGN_IN);
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if (requestCode == RC_SIGN_IN) {
+            IdpResponse response = IdpResponse.fromResultIntent(data);
+            FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+            if (resultCode == RESULT_OK && user != null) {
+                // Successfully signed in
+                binding.account.setText(user.getDisplayName());
+            } else {
+                // Sign in failed. If response is null the user canceled the
+                // sign-in flow using the back button. Otherwise check
+                // response.getError().getErrorCode() and handle the error.
+                // ...
+            }
+        }
     }
 }

aa57b6830e7f40f279c37ff822fa775bbfd1c9af
maevaI2! <maevaespana@orange.fr>
2021-01-27 17:13:53 +0100

    Display fragment changed to cardview

--- app/src/main/java/com/mespana/trans2021/ui/display/DisplayFragment.java
+++ app/src/main/java/com/mespana/trans2021/ui/display/DisplayFragment.java
@@ -1,6 +1,5 @@
 package com.mespana.trans2021.ui.display;
 
-import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
 import android.content.SharedPreferences;
@@ -13,15 +12,12 @@ import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.navigation.NavController;
 import androidx.navigation.Navigation;
 
 import com.mespana.trans2021.R;
 import com.mespana.trans2021.databinding.FragmentDisplayBinding;
 import com.mespana.trans2021.models.Artist;
 import com.mespana.trans2021.services.JsonParsingService;
-
-import java.util.Optional;
 
 public class DisplayFragment extends Fragment {
 
@@ -41,41 +37,58 @@ public class DisplayFragment extends Fragment {
         String recordId = sharedPref.getString(context.getString(R.string.shared_prefs_artist_rec_id), context.getString(R.string.unknown_artists));
         Artist artist = JsonParsingService.getArtistFromRecordId(recordId);
         binding = FragmentDisplayBinding.inflate(inflater, container, false);
-        if(artist != null) {
-            binding.artists.setText(artist.getArtistes());
-            binding.country.setText(artist.getOrigine_pays1());
-            binding.city.setText(artist.getOrigine_ville1());
-            binding.back.setOnClickListener(view -> Navigation.findNavController(view).navigate(R.id.action_displayFragment_to_tabsFragment));
-            String deezer = artist.getDeezer();
-            if(deezer == null) {
-                binding.deezer.setVisibility(View.GONE);
-            } else {
-                binding.deezer.setOnClickListener(v -> {
-                    // TODO lance le profil Deezer
-                });
-            }
-
-            String spotify = artist.getSpotify();
-            if(spotify == null) {
-                binding.spotify.setVisibility(View.GONE);
-            } else {
-                binding.spotify.setOnClickListener(v -> {
-                    // TODO lance le profil Spotify
-                    Intent intent = new Intent(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(spotify));
-                    this.startActivity(intent);
-
-                });
-            }
-            // si pas d'image récupérée, on met l'image de base (ic_profile)
-            //binding.roundedImage.setImageDrawable();
-            binding.roundedImage.setImageDrawable(getContext().getDrawable(R.drawable.samm_henshaw));
-        } else {
+        if(artist == null) {
             Toast.makeText(getContext(), "L'artiste n'existe pas", Toast.LENGTH_SHORT).show();
             Navigation.findNavController(getView()).navigate(R.id.action_displayFragment_to_tabsFragment);
         }
+        binding.edition.setText(artist.getEdition());
+        binding.listView.setAdapter(new PastEditionsListAdapter(artist.getEventList()));
+        binding.artists.setText(artist.getArtistes());
+        binding.country.setText(artist.getOrigine_pays1());
+        String ville = artist.getOrigine_ville1();
+        if(ville == null || ville.isEmpty()) binding.city.setVisibility(View.GONE);
+        else binding.city.setText(ville);
+        String deezer = artist.getDeezer();
+        if(deezer.isEmpty() || deezer == null) {
+            binding.deezer.setVisibility(View.GONE);
+        } else {
+            binding.deezer.setOnClickListener(v -> {
+                try {
+                    String url = "https://www.deezer.com/fr/album/";
+                    url+=deezer;
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                } catch (Exception e) {
+                    Toast.makeText(getContext(), "Vous n'avez pas d'application qui puisse permettre d'ouvrir le profil Deezer.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+
+        String spotify = artist.getSpotify();
+        if(spotify.isEmpty() || spotify == null) {
+            binding.spotify.setVisibility(View.GONE);
+        } else {
+            binding.spotify.setOnClickListener(v -> {
+                try {
+                    String url = "https://open.spotify.com/";
+                    String[] tokens = spotify.split(":");
+                    url+=tokens[1]+"/"+tokens[2];
+                    Intent i = new Intent(Intent.ACTION_VIEW);
+                    i.setData(Uri.parse(url));
+                    startActivity(i);
+                } catch (Exception e) {
+                    Toast.makeText(getContext(), "Vous n'avez pas d'application qui puisse permettre d'ouvrir le profil Spotify.", Toast.LENGTH_SHORT).show();
+                }
+            });
+        }
+        // si pas d'image récupérée, on met l'image de base (ic_profile)
+        //binding.roundedImage.setImageDrawable();
+        if(artist.isTriedToLoadImage()) binding.image.setImageBitmap(artist.getLoadedImage());
+        // TODO else try loading image
+        //else
+
         View root = binding.getRoot();
-        // TODO Use shared prefs to store index of clicked artist and then retrieve it inside the list
         return root;
     }
 }
---
+++ app/src/main/java/com/mespana/trans2021/ui/display/PastEditionsListAdapter.java
@@ -0,0 +1,48 @@
+package com.mespana.trans2021.ui.display;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.BaseAdapter;
+import android.widget.TextView;
+
+import com.mespana.trans2021.R;
+import com.mespana.trans2021.models.Event;
+
+import java.util.List;
+
+public class PastEditionsListAdapter extends BaseAdapter {
+
+    private List<Event> eventList;
+
+    public PastEditionsListAdapter(List<Event> eventList) {
+        this.eventList = eventList;
+    }
+
+    @Override
+    public int getCount() {
+        return eventList.size();
+    }
+
+    @Override
+    public Event getItem(int position) {
+        return eventList.get(position);
+    }
+
+    @Override
+    public long getItemId(int position) {
+        return 0;
+    }
+
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        if (convertView == null) {
+            convertView = LayoutInflater.from(parent.getContext()).inflate(R.layout.fragment_display_editions_list_item, parent, false);
+        }
+        Event e = getItem(position);
+        // TODO rémi date
+        ((TextView)convertView.findViewById(R.id.date)).setText(e.getDate().toString());
+        ((TextView)convertView.findViewById(R.id.place)).setText(e.getSalle());
+        return convertView;
+    }
+}

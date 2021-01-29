79aee46170ce6def08db75053d6a3d387ef51d26
Rousselle Flavien <flavien.rousselle@etudiant.univ-rennes1.fr>
2021-01-29 01:21:08 +0100

    Review Recherche

--- app/src/main/java/com/mespana/trans2021/ui/search/SearchFragment.java
+++ app/src/main/java/com/mespana/trans2021/ui/search/SearchFragment.java
@@ -8,23 +8,24 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+
 import androidx.fragment.app.Fragment;
 
+import com.mespana.trans2021.R;
 import com.mespana.trans2021.databinding.FragmentSearchBinding;
+import com.mespana.trans2021.models.Artist;
+import com.mespana.trans2021.services.JsonParsingService;
 import com.whiteelephant.monthpicker.MonthPickerDialog;
 
+import java.lang.reflect.Array;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
 
 
 public class SearchFragment extends Fragment {
     FragmentSearchBinding binding;
-    TextView year;
-    Button year_butt;
-
-    @Override
-    public void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-    }
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,36 +33,44 @@ public class SearchFragment extends Fragment {
         super.onCreate(savedInstanceState);
         binding = FragmentSearchBinding.inflate(inflater,container,false);
         View root = binding.getRoot();
-        year =  binding.txtYear;
-        year_butt = binding.thePicker;
         chooseYearOnly();
         return root;
+    }
 
-
+    private void setSearchButtonAction() {
+        ArrayList<String> artistIds = new ArrayList<>();
+        binding.searchButton.setOnClickListener(v -> {
+            if(binding.pickYear.getText() != null){
+                // TODO : Ajouter tous les recordId d'artistes avec annee == searchYear
+                // TODO : Remplacer par un spinner avec les années ? (Possibilité de reprendre le code de Shervin)
+            }
+            if(binding.originSearch.getText() != null) {
+                // TODO : Ajouter tous les recordId d'artistes avec annee == searchYear
+            }
+            if(binding.searchName.getText() != null) {
+                // TODO : Ajouter tous les recordId d'artistes avec annee == searchYear
+            }
+        });
+        // TODO : Afficher la liste des artistes trouvés via ListFragment.
     }
 
     private void chooseYearOnly() {
         final Calendar today = Calendar.getInstance();
 
-        year_butt.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                MonthPickerDialog.Builder builder = new MonthPickerDialog.Builder(getContext(),
-                        new MonthPickerDialog.OnDateSetListener() {
-                            @Override
-                            public void onDateSet(int selectedMonth, int selectedYear) {
-                                year.setText(String.valueOf(selectedYear));
-                            }
-                        }, today.get(Calendar.YEAR), today.get(Calendar.MONTH));
+        binding.pickYear.setOnClickListener(v -> {
+            MonthPickerDialog.Builder builder = new MonthPickerDialog.Builder(getContext(),
+                    (selectedMonth, selectedYear) -> binding.pickYear.setText(String.valueOf(selectedYear)),
+                    today.get(Calendar.YEAR),
+                    today.get(Calendar.MONTH));
 
-                builder.setActivatedMonth(Calendar.JULY)
-                        .setYearRange(1979, today.get(Calendar.YEAR))
-                        .setActivatedYear(today.get(Calendar.YEAR))
-                        .setTitle("(/^w^)/ ANNEE/YEAR")
-                        .showYearOnly()
-                        .build()
-                        .show();
-            }
+            builder.setActivatedMonth(Calendar.JULY)
+                    .setYearRange(1979, today.get(Calendar.YEAR))
+                    .setActivatedYear(today.get(Calendar.YEAR))
+                    .setTitle("Selection de l'année")
+                    .showYearOnly()
+                    .build()
+                    .show();
         });
-    }}
+    }
+}
 
